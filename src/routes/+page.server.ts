df2505c70826e49fe6944b3cca98153aac6f164f
Øyvind Malin <oyvindmal@outlook.com>
2026-04-25 19:08:36 +0200

    fix

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,10 @@
-import { STRAPI_TOKEN, STRAPI_URL } from '$env/static/private';
+import { STRAPI_URL } from '$env/static/private';
 import type { PageServerLoad } from './$types';
  import { strapi } from '@strapi/client';
 
 export const load = (async () => {
 	
-console.log(STRAPI_TOKEN);
+
 const client = strapi({ baseURL: 'http://cms.oyvindmal.no/api'});
 
 const homepage = client.single('front-page');
