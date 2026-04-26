196afb0c205254c0d126c85c8c08b91ea792029b
Øyvind Malin <oyvindmal@outlook.com>
2026-04-26 14:47:13 +0200

    Stylehseet

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import stil from '$lib/assets/style.css'
+    import { link } from 'fs';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link rel="stylesheet" href={stil} />
 </svelte:head>
-
+<h1>JAHA</h1>
 {@render children()}
