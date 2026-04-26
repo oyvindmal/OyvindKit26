39e2e48d834207c5037d97d990923e6b041a62b0
Øyvind Malin <oyvindmal@outlook.com>
2026-04-26 14:54:08 +0200

    fixing style import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
-	import stil from '$lib/assets/style.css'
+	import '$lib/assets/style.css'
     import { link } from 'fs';
 
 	let { children } = $props();
@@ -8,7 +8,7 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
-	<link rel="stylesheet" href={stil} />
+	
 </svelte:head>
 <h1>JAHA</h1>
 {@render children()}
